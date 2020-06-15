b801507b3338d54afc7cc7b817cd9f235728a5f4
ayainokuchi <r_aya_o.729@hotmail.co.jp>
2020-06-15 12:41:53 +0000

    11

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.admin')
+@extends('layouts.profile')
 
-@section('tittle','ニュースの新規作成')
+@section('tittle','プロフィールの編集')
 
 @section('content')
 
@@ -9,6 +9,48 @@
             <div class="col-md-8 mx-auto">
 
         <h1>Myプロフィール編集画面</h1>
+        <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+        <div class="form-group row">
+                         <label class="col-md-2">氏名</label>
+                         <div class="col-md-10">
+                             <input type="text" class="form-control" name="name" value={{ old('name') }}>
+                         </div>
+                     </div>
+                
+                <div class="form-group row">
+                         <label class="col-md-2">性別</label>
+                         <div class="col-md-10">
+                             <input type="text" class="form-control" name="gender" value={{ old('gender') }}>
+                         </div>
+                </div>
+                    
+                    <div class="form-group row">
+                         <label class="col-md-2">趣味</label>
+                         <div class="col-md-10">
+                             <input type="text" class="form-control" name="hobby" value={{ old('hobby') }}>
+                         </div> 
+                     </div>     
+                     
+                     <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
+                        </div>
+                     </div>    
+                     
+                     {{ csrf_field() }}
+                     <input type="submit" class="btn btn-primary" value="更新">
+                            
+                    </div>
+    </div>
+    </form>
     </div>
     </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,10 @@ Route::group(['prefix' => 'admin'], function(){
     Route::post('news/create','Admin\NewsController@create')->middleware('auth');
     Route::post('profile/create','Admin\ProfileController@create')->middleware('auth');
     Route::post('profile/edit','Admin\ProfileController@update')->middleware('auth');
-    
+    Route::get('news','Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit','Admin\NewsController@edit')->middleware('auth');
+    Route::post('news/edit','Admin\NewsController@update')->middleware('auth');
+    Route::get('news/delete','Admin\NewsController@delete')->middleware('auth');
     
     Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
